42c033e4ea93bef70f2a5e138c190d515500c510
Alison <alisonmozer@gmail.com>
2023-04-23 13:36:06 -0300

    Ajuste graficos

--- app.py
+++ app.py
@@ -42,17 +42,64 @@ def mostrar_grafico():
     df1 = df
     df1['Individual'] = df1['Individual'].str.rstrip('%').astype('float')
     df1['Individual'] = df1['Individual'].astype(float)
-    df_top10 = df1.nlargest(20, 'Individual')
-    # Cria um gráfico de barras
-    fig = go.Figure(data=[go.Bar(y=df_top10['Individual'].apply(lambda y: '{:.2f}%'.format(y)),x=df_top10['Material'].apply(lambda x: x.split()[0]))])
+    df_top10 = df1.nlargest(11, 'Individual')
 
-    # Adiciona um título ao gráfico
-    fig.update_layout(title='Meu gráfico de barras')
+    # Define as cores para as barras
+    cores = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf', '#aaffc3']
+
+    # Cria um gráfico de barras com rótulos e legenda
+    fig = go.Figure(data=[go.Bar(
+        y=df_top10['Individual'].apply(lambda y: '{:.2f}%'.format(y)),
+        x=df_top10['Material'],
+        marker_color=cores
+        )])
+    fig.update_layout(
+        title='Meu gráfico de barras',
+        xaxis_title='Materiais',
+        yaxis_title='Porcentagem Individual',
+        legend_title='Classificação',
+        plot_bgcolor='#f5f5f5', # Define a cor do fundo do gráfico
+        font=dict(size=16) # Define o tamanho da fonte dos rótulos
+    )
 
     # Converte o gráfico para um HTML que pode ser renderizado na interface web
     grafico_html = fig.to_html(full_html=False)
 
     return render_template('grafico.html', grafico_html=grafico_html)
+
+
+@app.route('/pizza')
+def mostrar_grafico_pizza():
+    df = pd.read_excel('exportada.xlsx')
+    df1 = df.copy()
+    df1['Individual'] = df1['Individual'].str.rstrip('%').astype('float')
+    df1['Individual'] = df1['Individual'].astype(float)
+    
+    # Agrupa os dados por classificação e calcula a soma dos valores individuais
+    df_agrupado = df1.groupby('Classificação').agg({'Individual': 'sum'})
+    
+    # Ordena as categorias pela soma dos valores individuais
+    df_agrupado = df_agrupado.sort_values(by='Individual', ascending=False)
+    
+    # Cria um gráfico de pizza
+    fig = go.Figure(data=[go.Pie(labels=df_agrupado.index, values=df_agrupado['Individual'], hole=0.3)])
+    
+    # Adiciona um título ao gráfico
+    fig.update_layout(title='Curva ABC por Classificação', 
+                      font=dict(size=18))
+    
+    # Adiciona informações nos rótulos do gráfico
+    fig.update_traces(textposition='inside', textinfo='percent+label')
+    
+    # Define as cores das fatias do gráfico
+    cores = ['rgb(215, 48, 39)', 'rgb(244, 109, 67)', 'rgb(253, 174, 97)', 'rgb(254, 224, 139)', 'rgb(217, 239, 139)', 
+             'rgb(166, 217, 106)', 'rgb(102, 189, 99)', 'rgb(26, 152, 80)']
+    fig.update_traces(marker=dict(colors=cores))
+    
+    # Converte o gráfico para um HTML que pode ser renderizado na interface web
+    grafico_html = fig.to_html(full_html=False)
+
+    return render_template('pizza.html', grafico_html=grafico_html)
 
 def executar_app():
     thread = threading.Thread(target=app.run)
